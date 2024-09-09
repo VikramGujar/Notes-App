d5aacadb544cc8d9fd737c47df7d3c084ebadbcf
VikramGujar <vikramgujar300@gmail.com>
2024-09-09 15:35:37 +0530

    functionality adedd Saving notes to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import NoteInput from "./NoteInput";
 import Note from "./Note";
 import Footer from "./Footer";
 
-function App(){
-    const [allNotes,setAllNotes] = useState([])
+function App() {
+  const [allNotes, setAllNotes] = useState(() => {
+    const savedNotes = localStorage.getItem("notes");
+    return savedNotes ? JSON.parse(savedNotes) : [];
+  });
 
-    function addNewNote(newNote){
-        setAllNotes(prevValue =>{
-            return [...prevValue,newNote];
-        });
-    }
-    
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(allNotes));
+  }, [allNotes]);
 
-    function deleteNote(id){
-        setAllNotes(prevValue => {
-            return prevValue.filter((item,index) =>{
-                return index !== id;
-            });
-            
-        })
-    }
+  function addNewNote(newNote) {
+    setAllNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
 
+  function deleteNote(id) {
+    setAllNotes((prevValue) => {
+      return prevValue.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-    return(
-        <div>
-            <Header />
-            <NoteInput 
-            onClick = {addNewNote}
+  return (
+    <div>
+      <Header />
+      <NoteInput onClick={addNewNote} />
+      <div className="main-note-container">
+        {allNotes.map((noteItem, index) => {
+          return (
+            <Note
+              key={index}
+              id={index}
+              title={noteItem.title}
+              description={noteItem.content}
+              dltClick={deleteNote}
             />
-            <div className="main-note-container">
-            {allNotes.map((noteItem,index) =>{
-                return <Note 
-                key = {index}
-                id = {index}
-                title = {noteItem.title}
-                description = {noteItem.content}
-                dltClick ={deleteNote}
-                />
-            })
-            }</div>
-            <Footer />
-        </div>
-    )
+          );
+        })}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,63 +1,58 @@
 import React, { useState } from "react";
 
+function NoteInput(props) {
+  const [noteInputes, setInputs] = useState({
+    title: "",
+    content: ""
+  });
 
-function NoteInput(props){
-    const [noteInputes,setInputs] = useState({
-        title : "",
-        content : ""
-    })
+  function handleNoteInput(event) {
+    const { name, value } = event.target;
 
-    function handleNoteInput(event){
-        const {name,value} = event.target;
+    setInputs((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
 
-        setInputs(prevValue =>{
-            return {
-                ...prevValue,
-                [name]:value
-            };
-        });
-    }
+  function noteAdd(event) {
+    props.onClick(noteInputes);
+    setInputs({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
 
-
-    function noteAdd(event){
-        props.onClick(noteInputes)
-        setInputs({
-            title:"",
-            content:""
-        })
-        event.preventDefault();
-    }
-
-    return(
-        <div className="note-input-container">
-        <div className="note-input">
-            <h1>Add your note</h1>
-            <form action="">
-            <input 
+  return (
+    <div className="note-input-container">
+      <div className="note-input">
+        <h1>Add your note</h1>
+        <form>
+          <input
             type="text"
-            name="title" 
+            name="title"
             placeholder="Note heading"
             onChange={handleNoteInput}
             value={noteInputes.title}
-            />
+          />
 
-
-            <input 
-            type="text" 
+          <input
+            type="text"
             name="content"
-            className="details" 
+            className="details"
             placeholder="Details of note"
             onChange={handleNoteInput}
             value={noteInputes.content}
-            />
-            
-            <button
-            onClick={noteAdd}
-            >Add</button>
-            </form>
-        </div>
-        </div>
-    )
+          />
+
+          <button onClick={noteAdd}>Add</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default NoteInput;
